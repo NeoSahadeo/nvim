1c6fbda825efb1375449bdbe69cad8ec0eec1a9e
NeoSahadeo <neosahadeo@protonmail.com>
2024-08-21 11:56:08 +0200

    :sparkles: added gdscript server attachement

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,5 +18,10 @@ return {
 				capabilities = capabilities,
 			})
 		end
+
+		require("lspconfig").gdscript.setup({
+			name = "godot",
+			cmd = vim.lsp.rpc.connect("127.0.0.1", "6005"),
+		})
 	end
 }
