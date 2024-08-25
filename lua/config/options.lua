d02e9b019c7c07f24d26e677e63710182974e6f6
NeoSahadeo <neosahadeo@protonmail.com>
2024-08-25 18:35:49 +0200

    :rocket: updated tab stops and added a comment

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,8 +4,8 @@ v.fillchars = 'eob: '
 v.clipboard = 'unnamedplus'
 v.undofile = true
 v.ruler = true
-v.tabstop = 4
-v.shiftwidth = 2
+-- v.tabstop = 4
+-- v.shiftwidth = 2
 
 vim.cmd('set nowrap')
 vim.cmd('set number')
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
 			enable = true,
 			additional_vim_regex_highlighting = false,
 		},
+		-- indent = { enable = true },
 	}
 end
 }
