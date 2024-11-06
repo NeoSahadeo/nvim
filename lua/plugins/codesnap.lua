8280ca88493f3c66a7c440783cf3797e22b2d18a
NeoSahadeo <neosahadeo@protonmail.com>
2024-11-06 20:12:47 +0000

    :sparkles: more stuff!

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,14 +1,21 @@
 return {
-	"mistricky/codesnap.nvim",
-	build = "make",
-	config = function ()
-		require("codesnap").setup({
-			mac_window_bar = false,
-			bg_theme = "sea",
-			watermark = "Neo Sahadeo",
-			bg_x_padding = 122,
-			bg_y_padding = 82,
-			bg_padding = nil
+	'mistricky/codesnap.nvim',
+	build = 'make',
+	config = function()
+		require('codesnap').setup({
+			mac_window_bar = true,
+			bg_color = '#000000',
+			bg_theme = 'sea',
+			watermark = 'Neo Sahadeo',
+			code_font_family = 'CaskaydiaCove Nerd Font',
+			watermark_font_family = 'Pacifico',
+			breadcrumbs_separator = '/',
+			has_breadcrumbs = false,
+			has_line_number = false,
+			show_workspace = false,
+			min_width = 0,
+			bg_x_padding = 0,
+			bg_y_padding = 0,
 		})
-	end
+	end,
 }
