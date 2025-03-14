9e2c64c1d940ca9e520a7892a3f1c7b63354689d
NeoSahadeo <neosahadeo@protonmail.com>
2025-03-14 18:09:11 +0000

    :rocket: small updates

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ require('config.options')
 require('config.keymaps')
 require('config.lazy')
 
-vim.cmd('colorscheme kanagawa-lotus')
+vim.cmd('colorscheme nightfox')
--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -10,15 +10,61 @@ return {
 			keywordStyle = { italic = true },
 			statementStyle = { bold = true },
 			typeStyle = {},
-			transparent = false, -- do not set background color
+			transparent = true, -- do not set background color
 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
 			colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				palette = {
+
+					lotusOrange = '#8F6702',
+					lotusInk1 = '#382e0f',
+					lotusYellow = '#382e0f',
+					lotusViolet4 = '#77219c',
+					lotusTeal1 = '#852267',
+					lotusTeal2 = '#026199',
+					lotusGreen = '#3D9C0E',
+					-- lotusGreen2 = '#3D9C0E',
+					lotusRed = '#c90407',
+					lotusGray3 = '#545454',
+					lotusYellow2 = '#c84053',
+				},
+				theme = {
+					wave = {},
+					lotus = {},
+					dragon = {},
+					all = {
+						ui = {
+							bg_gutter = 'none',
+						},
+					},
+				},
 			},
 			overrides = function(colors) -- add/modify highlights
-				return {}
+				local theme = colors.theme
+				return {
+
+					TelescopeTitle = { fg = theme.ui.special, bold = true },
+					TelescopePromptNormal = { bg = 'none' },
+					TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = 'none' },
+					TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = 'none' },
+					TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = 'none' },
+					TelescopePreviewNormal = { bg = 'none' },
+					TelescopePreviewBorder = { bg = 'none', fg = 'none' },
+
+					NormalFloat = { bg = 'none' },
+					FloatBorder = { bg = 'none' },
+					FloatTitle = { bg = 'none' },
+
+					-- Save an hlgroup with dark background and dimmed foreground
+					-- so that you can use it where your still want darker windows.
+					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+					-- Popular plugins that open floats will link to NormalFloat by default;
+					-- set their background accordingly if you wish to keep them dark and borderless
+					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+				}
 			end,
 			theme = 'wave', -- Load "wave" theme when 'background' option is not set
 			background = { -- map the value of 'background' option to a theme
