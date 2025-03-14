a84b289bdd58600ac8c60f8b7ee3edf4a5a7726d
NeoSahadeo <neosahadeo@protonmail.com>
2025-03-14 18:47:27 +0000

    :rocket: added some settings to the signature

--- lua/plugins/signature.lua
+++ lua/plugins/signature.lua
@@ -1,19 +1,29 @@
 return {
-	"ray-x/lsp_signature.nvim",
-	event = "VeryLazy",
-	opts = {},
+	'ray-x/lsp_signature.nvim',
+	event = 'VeryLazy',
+	opts = {
+		hint_prefix = {
+			above = '↙ ', -- when the hint is on the line above the current line
+			current = '← ', -- when the hint is on the same line
+			below = '↖ ', -- when the hint is on the line below the current line
+		},
+		transparency = nil, -- disabled by default, allow floating win transparent value 1~100
+		handler_opts = {
+			border = 'rounded', -- double, rounded, single, shadow, none, or a table of borders
+		},
+	},
 	config = function(_, opts)
-		require'lsp_signature'.setup(opts)
+		require('lsp_signature').setup(opts)
 	end,
-	init = function ()
-		vim.api.nvim_create_autocmd("LspAttach", {
+	init = function()
+		vim.api.nvim_create_autocmd('LspAttach', {
 			callback = function(args)
 				local bufnr = args.buf
 				local client = vim.lsp.get_client_by_id(args.data.client_id)
-				if vim.tbl_contains({ 'null-ls' }, client.name) then  -- blacklist lsp
+				if vim.tbl_contains({ 'null-ls' }, client.name) then -- blacklist lsp
 					return
 				end
-				require("lsp_signature").on_attach({
+				require('lsp_signature').on_attach({
 					-- ... setup options here ...
 				}, bufnr)
 			end,
@@ -21,5 +31,5 @@ return {
 		vim.keymap.set({ 'n', 'i', 'x', 'v' }, '<C-k>', function()
 			require('lsp_signature').toggle_float_win()
 		end, { silent = true, noremap = true, desc = 'toggle signature' })
-	end
+	end,
 }
