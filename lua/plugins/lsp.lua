ff71a2fd7ef7b08ca899d38d4b7f1e30cdb1988b
NeoSahadeo <neosahadeo@protonmail.com>
2025-05-14 00:26:29 +0100

    :hammer: Mason-LSP v2 API changes, will mod later

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,30 +8,5 @@ return {
 	init = function()
 		require('mason').setup()
 		require('mason-lspconfig').setup({})
-		local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-		require('mason-lspconfig').setup_handlers({
-			function(server_name)
-				require('lspconfig')[server_name].setup({
-					-- capabilities = capabilities,
-				})
-			end,
-			-- ['godot'] = function()
-			-- 	require('lspconfig').gdscript.setup({
-			-- 		name = 'godot',
-			-- 		cmd = vim.lsp.rpc.connect('127.0.0.1', '6005'),
-			-- 	})
-			-- end,
-			['jinja_lsp'] = function()
-				require('lspconfig').jinja_lsp.setup({
-					filetypes = {
-						'jinja',
-						'jinja2',
-						'j2',
-					},
-					capabilities = capabilities,
-				})
-			end,
-		})
 	end,
 }
