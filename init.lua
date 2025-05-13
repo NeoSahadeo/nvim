c38318a9b134f1f6bfad067ab60011e0f7150f97
NeoSahadeo <neosahadeo@protonmail.com>
2025-05-14 00:24:38 +0100

    :hammer: Added new colourscheme

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ require('config.keymaps')
 require('config.lazy')
 require('config.autocmds')
 
--- vim.cmd('set background=light')
-vim.cmd('colorscheme gruvbox')
+vim.cmd('set background=dark')
+vim.cmd('colorscheme impaled-nazarene')
+vim.cmd('highlight Normal ctermbg=NONE guibg=NONE')
