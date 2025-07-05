c71053af3351a6a74a0c9b878c4e763f95464376
NeoSahadeo <neosahadeo@protonmail.com>
2025-07-05 13:20:28 +0200

    :sparkles: changed coolours

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -1,14 +1,25 @@
-return{
-	"lukas-reineke/indent-blankline.nvim",
-	init = function ()
-		local has_file = vim.fn.expand('%') ~= ''
-		if has_file then
-			require("ibl").setup({
-				indent = {
-					char = "│",
-					tab_char = "│",
-				},
-			})
-		end
-	end
+return {
+	'lukas-reineke/indent-blankline.nvim',
+	main = 'ibl',
+	---@module "ibl"
+	---@type ibl.config
+	opts = {},
+	config = function()
+		local highlight = {
+			'BigBlackMen',
+		}
+
+		local hooks = require('ibl.hooks')
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, 'BigBlackMen', { fg = '#2F2F2F' })
+		end)
+
+		require('ibl').setup({
+			indent = {
+				highlight = highlight,
+				char = '·',
+				tab_char = '·',
+			},
+		})
+	end,
 }
