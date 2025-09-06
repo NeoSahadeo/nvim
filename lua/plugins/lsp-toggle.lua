303fe85e69a80295418811c9822b4283dbe6ad09
NeoSahadeo <neosahadeo@protonmail.com>
2025-09-06 22:44:13 +0200

    [local] Saving local config

--- lua/plugins/lsp-toggle.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- 'NeoSahadeo/lsp-toggle.nvim',
-	'lsp-toggle.nvim',
-	dir = '~/Documents/Projects/lsp-toggle.nvim',
-	dev = true,
-	config = function()
-		require('lsp-toggle').setup()
-	end,
-}
